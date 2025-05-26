ea047429f29bf3e737b4874d4b4f3ca37b855f01
segni_abera <segniab69@gmail.com>
2025-05-26 20:57:32 +0300

    Add initial hello.py script with greeting and character details

---
+++ lesson1/hello.py
@@ -0,0 +1,9 @@
+greeting = 'hello world'
+print(greeting)  
+
+character = 'john'
+age = 30;
+
+print("Character:", character)
+print("Age:", age)
+print("my name is " + character + " and I am " + str(age) + " years old.")
