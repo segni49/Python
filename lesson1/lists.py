e98fb63f43474446ece57b96c9e995062e42d039
segni_abera <segniab69@gmail.com>
2025-05-26 21:56:59 +0300

    Add functionality to print friends list and access elements in lists.py

--- lesson1/lists.py
+++ lesson1/lists.py
@@ -1,1 +1,7 @@
 friends = ["Alice", "Bob", "Charlie", "David"]
+print(friends)
+# Accessing elements in a list
+print(friends[0])  # Alice
+print(friends[1])  # Bob
+print(friends[2])  # Charlie
+print(friends[3])  # David
