63ae964c77ed033b0d159765fafca24cb08f2c94
segni_abera <segniab69@gmail.com>
2025-05-26 21:53:21 +0300

    Add MadLibGame.py for interactive mad lib generation

---
+++ lesson1/MadLibGame.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+madlib = f"Roses are {color},\n{plural_noun} are blue,\nI love {celebrity}."
+
+print(madlib)
