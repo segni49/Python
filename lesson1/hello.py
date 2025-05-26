6f2d177f45cf06741f686d391d31438af6285a11
segni_abera <segniab69@gmail.com>
2025-05-26 21:01:02 +0300

    Refactor hello.py to remove unnecessary semicolon and improve function definition

--- lesson1/hello.py
+++ lesson1/hello.py
@@ -2,8 +2,14 @@ greeting = 'hello world'
 print(greeting)  
 
 character = 'john'
-age = 30.538;
+age = 30.538
+ismale = True
 
 print("Character:", character)
 print("Age:", age)
 print("my name is " + character + " and I am " + str(age) + " years old.")
+
+def check() -> None:
+    if(ismale):
+        print("This character is male.")
+    print("This is a function that does nothing.")
