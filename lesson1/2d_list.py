5bf944131377e961efd83df9f6661ec0954bb344
segni_abera <segniab69@gmail.com>
2025-05-27 21:53:00 +0300

    Add 2D list implementation with element access examples

---
+++ lesson1/2d_list.py
@@ -0,0 +1,12 @@
+num_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12]
+]
+
+print (num_grid)
+# Accessing elements in a 2D list
+print(num_grid[0][0])  # Output: 1
+print(num_grid[1][1])  # Output: 5
+print(num_grid[2][2])  # Output: 9
